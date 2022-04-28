c44171a1ae6b9b8f1c9b94672073f07791985819
bunyamin <bunyaminusalam@gmail.com>
2022-04-28 12:21:35 +0200

    new push

--- capstone-app/src/component/navbar/navbar.js
+++ capstone-app/src/component/navbar/navbar.js
@@ -1,18 +1,41 @@
 import React from 'react'
+import {Nav, Navbar, Container} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 import { Link } from 'react-router-dom'
 import './navbar.css'
 
-
-export default function Navbar() {
+function NavBar() {
   return (
-    <div className='nav-div'>
-        <h2 className='nav-logo'>Renew</h2>
-        <ul className='nav-ul'>
-            <li className='nav-li'><Link to={'/'} className='nav-link'>Home</Link></li>
-            {/* <li className='nav-li'><Link to={'/contact'} className='nav-link'>Contact Us</Link></li> */}
-            <li className='nav-li'><Link to={'/about'} className='nav-link'>About</Link></li>
-            <li className='nav-li'><Link to={'/product'} className='nav-link'>Product</Link></li>
-        </ul>
+    <div className='nav'>
+        <Navbar bg='dark' variant='dark' fixed='top' expand='sm' >
+          <Container fluid>
+            <Navbar.Brand>
+              Alice
+            </Navbar.Brand>
+
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+            <Nav className='ms-auto'>
+              <Nav.Item>
+                <Nav.Link><Link to={'/'} className='nav-link'>Home</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to={'/product'} className='nav-link'>Product</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+              <Nav.Link><Link to={'/about'} className='nav-link'>About</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+              <Nav.Link><Link to={'/contact'} className='nav-link'>Contact</Link></Nav.Link>
+              </Nav.Item>
+            </Nav>
+            </Navbar.Collapse>
+          
+          </Container>
+          
+        </Navbar>
     </div>
   )
 }
+
+export default NavBar

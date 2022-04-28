2b9cf7186032a5d5ab160aeafaae4afb72862829
bunyamin <bunyaminusalam@gmail.com>
2022-04-28 12:27:20 +0200

    new push

--- capstone-app/src/component/home/home.js
+++ capstone-app/src/component/home/home.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import './home.css'
 function Home() {
   return (
     <div>
-        <h1>This is home page</h1>
+      <div className='header-div'>
+        <h2 className='header-h2'>
+          enjoy our delicious meal
+        </h2>
+        <p className='header-p'>
+          Our restaurant is one of the greatest places to enjoy breakfast, launch, or dinner. Learn more about our menu.
+        </p>
+      </div>
+      <div className='about-div'>
+        <h4>about us</h4>
+        <h2>
+          welcome to our restaurant
+        </h2>
+        <p className=''>
+        Protective masks and facial coverings will have to be worn by all staff and guests. Masks are required at all times at the restaurant unless seated at the table.<br/>
+        Reservations are strongly recommended since we are only currently operating at a smaller capacity.
+        </p>
+      </div>
     </div>
   )
 }

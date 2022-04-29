55192daba0ffdd64e3725377f757143dadc3fb5d
bunyamin <bunyaminusalam@gmail.com>
2022-04-29 02:36:25 +0200

    new push

--- capstone-app/src/App.js
+++ capstone-app/src/App.js
@@ -26,7 +26,7 @@ function App() {
             <Route path='/about' element={<About />} />
             <Route path='/contact' element={<Contact />} />
         </Routes> 
-        {/* <Footer /> */}
+        <Footer />
       </Router>
     </div>
   );

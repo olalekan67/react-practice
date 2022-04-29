a060b8e1fe1703fe7dddd13718c18de545137744
bunyamin <bunyaminusalam@gmail.com>
2022-04-29 03:19:04 +0200

    new push

---
+++ capstone-app/src/component/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+  return (
+    <footer className='footer-div'>
+        <h4 className='footer-h4'>
+            &copy; 2022 Alice. All right reserved.
+        </h4>
+    </footer>
+  )
+}
+
+export default Footer
